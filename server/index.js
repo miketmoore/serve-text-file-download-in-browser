daf79183df4d26422a07be8aae980a06785ad4bf
Michael Moore <michael.moore@contrastsecurity.com>
2018-10-19 19:44:24 -0400

    request to download a file

--- server/index.js
+++ server/index.js
@@ -4,4 +4,9 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/download', (req, res) => {
+  var file = __dirname + '/data/hello.txt';
+  res.download(file); // Set disposition and send it.
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
